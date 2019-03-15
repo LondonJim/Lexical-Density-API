dae2d3704b25ad0b1ae6bb969f2f7b19055d6934
James Sutherland <jrsutherland78@googlemail.com>
2019-03-15 15:27:33 +0000

    add post route returns data

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const app = express()
 
-app.get('/complexity', (req, res) => {
-  res.send("hello world")
+app.use(express.json())
+
+app.post('/complexity', (req, res) => {
+  return res.status(200).json( { data: req.body.sentences })
 })
 
 module.exports = app

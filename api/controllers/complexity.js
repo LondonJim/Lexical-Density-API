4f33242f9a4c39b0b0096bdc17153dd9e41bc86a
James Sutherland <jrsutherland78@googlemail.com>
2019-03-15 16:45:01 +0000

    add read file word list and posted sentences

--- api/controllers/complexity.js
+++ api/controllers/complexity.js
@@ -1,9 +1,17 @@
 let fs = require('fs')
 
 exports.complexity = ((req, res) => {
+  let sentences = req.body.sentences
+
   fs.readFile('./nonLexicalWords.txt', function(err, data) {
     if(err) throw err;
     let nonLexical = data.toString().split("\n");
-    return res.status(200).json( { message: nonLexical })
-  });
+    displayData(nonLexical)
+  })
+
+  displayData = (nonLexical) => {
+    return res.status(200).json( { list: nonLexical,
+                                   sentence: sentences})
+  }
+
 })

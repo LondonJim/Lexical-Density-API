fa70d75d869a60e3d40ea6787be60930adbe3fbf
James Sutherland <jrsutherland78@googlemail.com>
2019-03-16 20:13:46 +0000

    add density check return json for one sentence

--- api/controllers/complexity.js
+++ api/controllers/complexity.js
@@ -10,8 +10,32 @@ exports.complexity = ((req, res) => {
   })
 
   displayData = (nonLexical) => {
-    return res.status(200).json( { list: nonLexical,
-                                   sentence: sentences})
+    let wordsArray = words()
+    let totalWords = wordsArray.length
+    let totalNonLexicalWords = checkNonLexical(wordsArray, nonLexical)
+    let density = nonLexicalDensity(totalWords, totalNonLexicalWords)
+
+    return res.status(200).json( { data: { overall_density: density } })
+  }
+
+  words = () => {
+    if (sentences[sentences.length - 1] === ".") sentences = sentences.slice(0, -1)
+    let wordsArray = sentences.split(" ")
+    return wordsArray
+  }
+
+  checkNonLexical = (words, nonLexical) => {
+    let totalNonLexical = 0
+    for (let i=0; i < words.length - 1; i++) {
+      for (let j=0; j < nonLexical.length - 1; j++) {
+        if (words[i] === nonLexical[j]) totalNonLexical ++
+      }
+    }
+    return totalNonLexical
+  }
+
+  nonLexicalDensity = (totalWords, totalNonLexicalWords) => {
+    return Math.round((totalNonLexicalWords / totalWords) * 100) / 100
   }
 
 })

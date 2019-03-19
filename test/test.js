e25746efb56a61b6d1eba118d4589eb98c8c0688
James Sutherland <jrsutherland78@googlemail.com>
2019-03-19 17:28:01 +0000

    add test for post tom compexity route

---
+++ test/test.js
@@ -0,0 +1,25 @@
+const chai = require('chai')
+const chaiHttp = require('chai-http')
+const should = chai.should()
+const server = require('../server')
+
+chai.use(chaiHttp)
+
+describe('Complexity', () => {
+
+  describe('post', () => {
+    it('should return the lexical density of sentence in json', (done) => {
+      let sentence = { sentences: "Kat likes going to the cinema" }
+
+      chai.request(server)
+        .post('/complexity')
+        .send(sentence)
+        .end((err, res) => {
+          should.exist(res.body)
+          res.should.have.status(200)
+          res.body.should.have.property('data').eql({'overall_density': 0.67})
+          done()
+        })
+    })
+  })
+})

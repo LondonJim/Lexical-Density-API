7b542b2f8bf7b35fcb5f4dca72726cadd9021613
James Sutherland <jrsutherland78@googlemail.com>
2019-03-19 12:52:14 +0000

    extract model DisplayData from controller

--- api/controllers/complexity.js
+++ api/controllers/complexity.js
@@ -1,68 +1,13 @@
-let fs = require('fs')
+const displayData = require('../models/complexity')
+const DisplayData = displayData.DisplayData
 
 exports.complexity = ((req, res) => {
-
-  class DisplayData {
-
-    constructor(req, res) {
-      this.req = req
-      this.res = res
-      this.sentenceDensities = []
-      this.sentences = this.req.body.sentences.toLowerCase()
-    }
-
-    executeDisplay() {
-      fs.readFile('./nonLexicalWords.txt', function(err, data) {
-        if(err) throw err
-        let nonLexical = data.toString().split("\n")
-        this.checkQuery()
-        this.displayData(nonLexical)
-      }.bind(this))
-    }
-
-    checkQuery() {
-      if (this.req.query.mode === 'verbose') {
-        this.showSentenceDensities = true
-      } else {
-        this.showSentenceDensities = false
-      }
-    }
-
-    displayData(nonLexical) {
-      let wordsArray = this.createWordsArray()
-      let totalWords = wordsArray.length
-
-      let totalNonLexicalWords = this.checkNonLexical(wordsArray, nonLexical)
-
-      let totalLexicalWords = totalWords - totalNonLexicalWords
-      let density = this.lexicalDensity(totalWords, totalLexicalWords)
-
-      return this.res.status(200).json( { data: { overall_density: density } })
-    }
-
-    createWordsArray() {
-      this.sentencesArray = this.sentences.split(".")
-      if (this.sentencesArray[this.sentencesArray.length - 1] === "") this.sentencesArray.pop()
-      let wordsArray = this.sentencesArray.join("")
-      return wordsArray.split(" ")
-    }
-
-    checkNonLexical(words, nonLexical) {
-      let totalNonLexical = 0
-      for (let i=0; i < words.length; i++) {
-        for (let j=0; j < nonLexical.length; j++) {
-          if (words[i] === nonLexical[j]) totalNonLexical ++
-        }
-      }
-      return totalNonLexical
-    }
-
-    lexicalDensity(totalWords, totalNonLexicalWords) {
-      return Math.round((totalNonLexicalWords / totalWords) * 100) / 100
-    }
-  }
-
-  displayData = new DisplayData(req, res)
-  displayData.executeDisplay()
-
+  let display
+  let sentences = req.body.sentences
+  let query = req.query.mode
+  let displayData = new DisplayData(sentences, query);
+  (async () => {
+    display = await displayData.executeDisplay()
+    res.status(200).json( { data: { overall_density: display } })
+  })()
 })
---
+++ api/models/complexity.js
@@ -0,0 +1,71 @@
+const fs = require('fs')
+
+class DisplayData {
+
+  constructor(sentences, query) {
+    this.sentenceDensities = []
+    this.query = query
+
+    this.density = 0
+    this.sentences = sentences.toLowerCase()
+  }
+
+  executeDisplay() {
+    return new Promise(function(resolve, reject) {
+      let executing = fs.readFile('./nonLexicalWords.txt', function(err, data) {
+        if(err) throw err
+        let nonLexical = data.toString().split("\n")
+        this.checkQuery()
+        resolve(this.displayData(nonLexical))
+      }.bind(this))
+    }.bind(this))
+  }
+
+  checkQuery() {
+    if (this.query === 'verbose') {
+      this.showSentenceDensities = true
+    } else {
+      this.showSentenceDensities = false
+    }
+  }
+
+  displayData(nonLexical) {
+    let wordsArray = this.createWordsArray()
+    let totalWords = wordsArray.length
+
+    let totalNonLexicalWords = this.checkNonLexical(wordsArray, nonLexical)
+
+    let totalLexicalWords = totalWords - totalNonLexicalWords
+    let density = this.lexicalDensity(totalWords, totalLexicalWords)
+
+    this.density = density
+
+    return this.density
+  }
+
+  createWordsArray() {
+    this.sentencesArray = this.sentences.split(".")
+    if (this.sentencesArray[this.sentencesArray.length - 1] === "") this.sentencesArray.pop()
+    let wordsArray = this.sentencesArray.join("")
+    return wordsArray.split(" ")
+  }
+
+  checkNonLexical(words, nonLexical) {
+    let totalNonLexical = 0
+    for (let i=0; i < words.length; i++) {
+      for (let j=0; j < nonLexical.length; j++) {
+        if (words[i] === nonLexical[j]) totalNonLexical ++
+      }
+    }
+    return totalNonLexical
+  }
+
+  lexicalDensity(totalWords, totalNonLexicalWords) {
+    return Math.round((totalNonLexicalWords / totalWords) * 100) / 100
+  }
+}
+
+// displayData = new DisplayData(req, res)
+// displayData.executeDisplay()
+
+module.exports = { DisplayData: DisplayData }

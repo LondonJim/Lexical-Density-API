bb4cbd2b6d0ae0867698099482f0b16be9e5e266
James Sutherland <jrsutherland78@googlemail.com>
2019-03-22 17:33:18 +0000

    add header checks, route error message

--- app.js
+++ app.js
@@ -5,6 +5,32 @@ const complexityRoutes = require('./api/routes/complexity')
 
 app.use(express.json())
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization')
+  if (req.method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET')
+    return res.status(200).json({})
+  }
+  next()
+})
+
 app.post('/complexity', complexityRoutes)
+
+app.use((req, res, next) => {
+  const error = new Error('not found')
+  error.status = 404
+  next(error)
+})
+
+app.use((error, req, res, next) => {
+  res.status(error.status || 500)
+  res.json({
+    error: {
+      message: error.message
+    }
+  })
+})
 
 module.exports = app

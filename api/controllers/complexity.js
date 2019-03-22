e3f6353897a0c8cb2b476de9b59a7f1fef15d7a8
James Sutherland <jrsutherland78@googlemail.com>
2019-03-22 17:11:07 +0000

    remove named variables from complexity controller

--- api/controllers/complexity.js
+++ api/controllers/complexity.js
@@ -4,24 +4,21 @@ const lexicalDensity = require('../models/lexicalDensity')
 const LexicalDensity = lexicalDensity.LexicalDensity
 
 exports.complexity = ((req, res, next) => {
-  let lexicalDensity
-  let words
-  let density
   let sentences = req.body.sentences
   let query = req.query.mode;
 
   (async () => {
     try {
-      words = await loadWords()
-      lexicalDensity = new LexicalDensity(sentences, query, words)
-      density = await lexicalDensity.main()
+      let words = await loadWords()
+      let lexicalDensity = new LexicalDensity(sentences, query, words)
+      let density = await lexicalDensity.main()
       if (query === 'verbose') {
         res.status(200).json( { data: { sentence_density: density.sentences, overall_density: density.overall } })
       } else {
         res.status(200).json( { data: { overall_density: density.overall } })
       }
     } catch(event) {
-      res.status(500).json( { error: event.stack } )
+      res.status(500).json( { error: 'Something went wrong' } )
     }
   })();
 })
